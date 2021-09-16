d6ec76c51a549f547113d8fe500e1e31be62a294
ElyesMaamouri <elyesmaamouri@gmail.com>
2021-09-16 11:54:30 +0100

    Add Grouping Operations With Tags

--- routes/books.js
+++ routes/books.js
@@ -45,7 +45,7 @@ const idLength = 8;
  * /books:
  *   get:
  *     summary: Returns the list of all the books
- 
+ *     tags: [Books]
  *     responses:
  *       200:
  *         description: The list of the books
