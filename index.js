a9ba1f527e03f255a90ea9cee7b5ba248858bda7
ElyesMaamouri <elyesmaamouri@gmail.com>
2021-09-16 10:44:01 +0100

    Add config initial config  swagger

--- index.js
+++ index.js
@@ -11,6 +11,27 @@ import router from "./routes/books.js";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
+import swaggerUI from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library API",
+      version: "1.0.0",
+      description: "A simple Express Library API",
+    },
+    servers: [
+      {
+        url: "http://localhost:4000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+const specs = swaggerJSDoc(options);
+
 const PORT = process.env.PORT || 4000;
 const __filename = fileURLToPath("file://db.json", import.meta.url);
 const file = dirname(__filename);
@@ -21,6 +42,7 @@ const db = new Low(adapter);
 db.write({ books: [] });
 
 const app = express();
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 app.db = db;
 app.use(cors());
